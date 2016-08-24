b2b467837a5da7f236e52b03a1c5e1869e1d9388
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-08-24 15:17:06 +0200

    add test view to allow testing with django client integration

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
+from testapp.views import number_of_queries_to_run_view
 
 
 urlpatterns = patterns(
     'testapp',
+    url(
+        r'number_of_queries_to_run/(?P<nr_of_queries>\d+)',
+        number_of_queries_to_run_view, {}, 'nr_of_queries_view'),
 )
