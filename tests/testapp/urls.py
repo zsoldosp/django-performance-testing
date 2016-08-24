1f4d502362c69adbd347d7a25d215ae4043942c9
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-08-24 16:32:22 +0200

    asof django 1.8, there is no need for patterns in urls.py

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from testapp.views import number_of_queries_to_run_view
 
 
-urlpatterns = patterns(
-    'testapp',
+urlpatterns = [
     url(
         r'number_of_queries_to_run/(?P<nr_of_queries>\d+)',
         number_of_queries_to_run_view, {}, 'nr_of_queries_view'),
-)
+]
