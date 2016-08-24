c54157fb33ffd86baf37af3d42b515b239c4b98e
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-08-24 15:33:36 +0200

    for failure, provide the request.PATH int he message

--- tests/testapp/tests/test_integrates_with_django_test_client.py
+++ tests/testapp/tests/test_integrates_with_django_test_client.py
@@ -1,3 +1,4 @@
+import pprint
 import pytest
 from django.core.urlresolvers import reverse
 
@@ -6,6 +7,9 @@ def test_can_specify_limits_through_settings_for_django_test_client(
         db, settings, client):
     settings.PERFORMANCE_LIMITS = {
         'django.test.client.Client': {'count_limit': 0}}
+    url = reverse('nr_of_queries_view', kwargs={'nr_of_queries': 1})
     with pytest.raises(ValueError) as excinfo:
-        client.get(reverse('nr_of_queries_view', kwargs={'nr_of_queries': 1}))
-    assert 'Too many (1) queries (limit: 0)' == str(excinfo.value)
+        client.get(url)
+    expected_error_message = 'Too many (1) queries (limit: 0) {}'.format(
+        pprint.pformat({'Client.GET': url}))
+    assert expected_error_message == str(excinfo.value)
