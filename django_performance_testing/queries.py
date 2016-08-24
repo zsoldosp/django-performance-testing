4962ea9019241bce0be4391f9817f5beb80adc77
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-08-24 13:37:15 +0200

    can capture CRUD queries issued through QuerySets

--- django_performance_testing/queries.py
+++ django_performance_testing/queries.py
@@ -5,11 +5,11 @@ class QueryCollector(object):
 
     def __enter__(self):
         self.queries = []
+        self.nr_of_queries_when_entering = len(connection.queries)
         # TODO: need to assert it's back to normal
         connection.force_debug_cursor = True
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # TODO: needs to be a copy
         # TODO: nested decorators/ctx managers
-        self.queries = connection.queries
+        self.queries = connection.queries[self.nr_of_queries_when_entering:]
--- tests/testapp/tests/test_limits_on_number_of_queries.py
+++ tests/testapp/tests/test_limits_on_number_of_queries.py
@@ -2,7 +2,19 @@ from django.contrib.auth.models import Group
 from django_performance_testing.queries import QueryCollector
 
 
-def test_can_count_inserts(db):
-    with QueryCollector() as qc:
+def test_captures_queries(db):
+    with QueryCollector() as qc_insert:
         Group.objects.create(name='foo')
-    assert len(qc.queries) == 1
+    assert len(qc_insert.queries) == 1
+    with QueryCollector() as qc_select:
+        list(Group.objects.all())
+    assert len(qc_select.queries) == 1
+    with QueryCollector() as qc_update:
+        Group.objects.update(name='bar')
+    assert len(qc_update.queries) == 1
+    with QueryCollector() as qc_delete:
+        Group.objects.all().delete()
+    delete_queries = list(
+        x['sql'] for x in qc_delete.queries
+        if x['sql'].startswith("QUERY = 'DELETE"))
+    assert len(delete_queries) != 0
