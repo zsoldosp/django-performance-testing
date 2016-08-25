862d0f9bbfc203a48d8fcb7f5459c8edb90aab79
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-08-25 11:27:28 +0200

    cosmetic - remove unneeded blank lines

--- tests/testapp/tests/test_can_classify_queries_into_read_and_write_other.py
+++ tests/testapp/tests/test_can_classify_queries_into_read_and_write_other.py
@@ -5,7 +5,6 @@ from django_performance_testing.queries import classify_query
 @pytest.mark.parametrize('sql', [
     'QUERY = u\'SELECT "auth_group"."id", "auth_group"."name" FROM "auth_group"\' - PARAMS = ()',  # noqa
     'QUERY = \'SELECT "auth_group"."id", "auth_group"."name" FROM "auth_group"\' - PARAMS = ()',  # noqa
-
 ], ids=['py2', 'py3'])
 def test_can_classify_select(sql):
     assert 'read' == classify_query(sql)
@@ -14,7 +13,6 @@ def test_can_classify_select(sql):
 @pytest.mark.parametrize('sql', [
     'QUERY = \'INSERT INTO "auth_group" ("name") VALUES (%s)\' - PARAMS = (\'foo\',)',  # noqa
     'QUERY = u\'INSERT INTO "auth_group" ("name") VALUES (%s)\' - PARAMS = (\'foo\',)',  # noqa
-
 ], ids=['py2', 'py3'])
 def test_can_classify_insert(sql):
     assert 'write' == classify_query(sql)
@@ -23,7 +21,6 @@ def test_can_classify_insert(sql):
 @pytest.mark.parametrize('sql', [
     'QUERY = \'UPDATE "auth_group" SET "name" = %s\' - PARAMS = (\'bar\',)',  # noqa
     'QUERY = u\'UPDATE "auth_group" SET "name" = %s\' - PARAMS = (\'bar\',)',  # noqa
-
 ], ids=['py2', 'py3'])
 def test_can_classify_update(sql):
     assert 'write' == classify_query(sql)
