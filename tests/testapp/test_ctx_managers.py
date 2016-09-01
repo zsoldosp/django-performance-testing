fcc552e8ba732efe670241a6f93fc3ee1a516a7c
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-09-01 14:35:59 +0200

    refactoring: move capture_result_collected to helper module

--- tests/testapp/test_ctx_managers.py
+++ tests/testapp/test_ctx_managers.py
@@ -1,4 +1,21 @@
 from django_performance_testing import context
+from django_performance_testing.signals import result_collected
+
+
+class capture_result_collected(object):
+
+    def __enter__(self):
+        self.calls = []
+        result_collected.connect(self.result_collected_handler)
+        return self
+
+    def result_collected_handler(self, signal, sender, result, context):
+        self.calls.append(dict(
+            sender=sender, signal=signal, result=result,
+            context=context))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        result_collected.disconnect(self.result_collected_handler)
 
 
 class override_current_context(object):
