d6a4433c13c045892c768f622806dd6b69c83b23
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-09-01 11:05:58 +0200

    minor: don't duplicate prod code logic in test (use hardcoded value instead)

--- tests/testapp/tests/test_integrates_with_django_test_client.py
+++ tests/testapp/tests/test_integrates_with_django_test_client.py
@@ -1,13 +1,12 @@
-import pprint
 import pytest
 from django.core.urlresolvers import reverse
 from django.db import reset_queries
 
 
 @pytest.mark.parametrize('kwparams', [
-    {'method': 'get', 'limit': 4, 'queries': 5},
-    {'method': 'post', 'limit': 1, 'queries': 2},
-], ids=['get', 'post'])
+    {'method': 'GET', 'limit': 4, 'queries': 5},
+    {'method': 'POST', 'limit': 1, 'queries': 2},
+], ids=['GET', 'POST'])
 def test_can_specify_limits_through_settings_for_django_test_client(
         db, settings, client, kwparams):
     reset_queries()  # without this, things go wrong with counting
@@ -17,8 +16,11 @@ def test_can_specify_limits_through_settings_for_django_test_client(
     url = reverse(
         'nr_of_queries_view', kwargs={'nr_of_queries': kwparams['queries']})
     with pytest.raises(ValueError) as excinfo:
-        getattr(client, kwparams['method'])(url)
-    expected_error_message = 'Too many ({}) queries (limit: {}) {}'.format(
-        kwparams['queries'], kwparams['limit'],
-        pprint.pformat({'Client.{}'.format(kwparams['method'].upper()): url}))
+        getattr(client, kwparams['method'].lower())(url)
+    expected_error_message = \
+        'Too many ({queries}) queries (limit: {limit}) ' \
+        '{{\'Client.{method}\': {url!r}}}'.format(
+            url=url,
+            **kwparams
+        )
     assert expected_error_message == str(excinfo.value)
