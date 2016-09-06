d128c713b9a60443c283829c4aea5e653cd7c848
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-09-06 16:50:22 +0200

    introduce DelegatingProxy object
    based on django.db.connection proxy class

--- django_performance_testing/utils.py
+++ django_performance_testing/utils.py
@@ -9,3 +9,33 @@ class BeforeAfterWrapper(object):
     def wrap(self, *a, **kw):
         with self.context_manager:
             return self.orig_method(*a, **kw)
+
+
+class DelegatingProxy(object):
+    """
+    Proxy for accessing the wrapped object's attributes, while allowing
+    overwriting specific methods
+    """
+    def __init__(self, wrapped):
+        self.__dict__['wrapped'] = wrapped
+
+    def __getattr__(self, item):
+        return getattr(self.wrapped, item)
+
+    def __setattr__(self, name, value):
+        return setattr(self.wrapped, name, value)
+
+    def __delattr__(self, name):
+        return delattr(self.wrapped, name)
+
+    def __eq__(self, other):
+        return self.wrapped == other
+
+    def __ne__(self, other):
+        return self.wrapped != other
+
+    def __len__(self):
+        return len(self.wrapped)
+
+    def __iter__(self):
+        return iter(self.wrapped)
