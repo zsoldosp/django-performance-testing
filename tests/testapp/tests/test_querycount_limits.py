354f2ff82cfac0827d1a0bb6ff4741f308240de9
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-09-09 14:50:50 +0200

    remove the now unneeded class, as there is a whole module

--- tests/testapp/tests/test_querycount_limits.py
+++ tests/testapp/tests/test_querycount_limits.py
@@ -10,44 +10,48 @@ def qcr(n):
     return QueryCountResult(name='total', queries=range(n))
 
 
-class TestQueryBatchLimit(object):
-    def test_it_is_a_properly_wired_up_base_limit(self):
-        assert issubclass(QueryBatchLimit, BaseLimit)
-        assert QueryBatchLimit.collector_cls == QueryCollector
-        assert QueryBatchLimit.results_collected_handler == \
-            BaseLimit.results_collected_handler
+def test_it_is_a_properly_wired_up_base_limit():
+    assert issubclass(QueryBatchLimit, BaseLimit)
+    assert QueryBatchLimit.collector_cls == QueryCollector
+    assert QueryBatchLimit.results_collected_handler == \
+        BaseLimit.results_collected_handler
 
-    def test_when_exactly_limit_no__error(self):
-        limit = QueryBatchLimit(count_limit=1)
-        limit.handle_results(results=[qcr(1)], context=None)
-        assert True  # no exception raised
 
-    def test_when_below_limit_no__error(self):
-        limit = QueryBatchLimit(count_limit=3)
-        limit.handle_results(results=[qcr(0)], context=None)
-        assert True  # no exception raised
+def test_when_exactly_limit_no__error():
+    limit = QueryBatchLimit(count_limit=1)
+    limit.handle_results(results=[qcr(1)], context=None)
+    assert True  # no exception raised
 
-    def test_when_above_limit_it_fails_with_meaningful_error_message(self):
-        limit = QueryBatchLimit(count_limit=2)
-        with pytest.raises(ValueError) as excinfo:
-            limit.handle_results(results=[qcr(3)], context=None)
-        assert 'Too many (3) queries (limit: 2)' == str(excinfo.value)
 
-    def test_given_context_it_is_included_in_error_message(self):
-        limit = QueryBatchLimit(count_limit=3)
-        with pytest.raises(ValueError) as excinfo:
-            limit.handle_results(
-                results=[qcr(4)], context={'extra': 'context'})
-        assert 'Too many (4) queries (limit: 3) {\'extra\': \'context\'}' == \
-            str(excinfo.value)
+def test_when_below_limit_no__error():
+    limit = QueryBatchLimit(count_limit=3)
+    limit.handle_results(results=[qcr(0)], context=None)
+    assert True  # no exception raised
 
-    def test_integration_test_with_db(self, db):
-        with pytest.raises(ValueError) as excinfo:
-            with override_current_context() as ctx:
-                with QueryBatchLimit(count_limit=2):
-                    ctx.enter(key='some', value='context')
-                    list(Group.objects.all())
-                    Group.objects.update(name='bar')
-                    Group.objects.create(name='group')
-        assert 'Too many (3) queries (limit: 2) {\'some\': [\'context\']}' in \
-            str(excinfo.value)
+
+def test_when_above_limit_it_fails_with_meaningful_error_message():
+    limit = QueryBatchLimit(count_limit=2)
+    with pytest.raises(ValueError) as excinfo:
+        limit.handle_results(results=[qcr(3)], context=None)
+    assert 'Too many (3) queries (limit: 2)' == str(excinfo.value)
+
+
+def test_given_context_it_is_included_in_error_message():
+    limit = QueryBatchLimit(count_limit=3)
+    with pytest.raises(ValueError) as excinfo:
+        limit.handle_results(
+            results=[qcr(4)], context={'extra': 'context'})
+    assert 'Too many (4) queries (limit: 3) {\'extra\': \'context\'}' == \
+        str(excinfo.value)
+
+
+def test_integration_test_with_db(db):
+    with pytest.raises(ValueError) as excinfo:
+        with override_current_context() as ctx:
+            with QueryBatchLimit(count_limit=2):
+                ctx.enter(key='some', value='context')
+                list(Group.objects.all())
+                Group.objects.update(name='bar')
+                Group.objects.create(name='group')
+    assert 'Too many (3) queries (limit: 2) {\'some\': [\'context\']}' in \
+        str(excinfo.value)
