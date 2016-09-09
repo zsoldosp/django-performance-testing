cfad547cbf629a19b5bfdfb0ca846d851ba2b92b
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-09-09 16:01:01 +0200

    use parametrization to add typed test to existing limit tests

--- tests/testapp/tests/test_querycount_limits.py
+++ tests/testapp/tests/test_querycount_limits.py
@@ -6,8 +6,12 @@ from django_performance_testing.core import BaseLimit
 from testapp.test_helpers import override_current_context
 
 
-def qcr(n):
-    return QueryCountResult(name='total', queries=range(n))
+def wrapped_between_irrelevant_results(name, n):
+    return [
+        QueryCountResult(name='dont check before', queries=range(n + 1)),
+        QueryCountResult(name=name, queries=range(n)),
+        QueryCountResult(name='dont check after', queries=range(n + 2)),
+    ]
 
 
 def test_it_is_a_properly_wired_up_base_limit():
@@ -17,32 +21,65 @@ def test_it_is_a_properly_wired_up_base_limit():
         BaseLimit.results_collected_handler
 
 
-def test_when_exactly_limit_no__error():
-    limit = QueryBatchLimit(total=1)
-    limit.handle_results(results=[qcr(1)], context=None)
+@pytest.mark.parametrize('name,limit', [
+    ('total', 1), ('total', 3),
+    ('read', 3), ('read', 2),
+    ('write', 6), ('write', 9),
+])
+def test_when_exactly_limit_no__error(name, limit):
+    qlimit = QueryBatchLimit(**{name: limit})
+    qlimit.handle_results(
+        results=wrapped_between_irrelevant_results(name, limit), context=None)
     assert True  # no exception raised
 
 
-def test_when_below_limit_no__error():
-    limit = QueryBatchLimit(total=3)
-    limit.handle_results(results=[qcr(0)], context=None)
+@pytest.mark.parametrize('name,limit,queries', [
+    ('total', 1, 0), ('total', 3, 2),
+    ('read', 3, 2), ('read', 2, 1),
+    ('write', 6, 4), ('write', 9, 7),
+])
+def test_when_below_limit_no__error(name, limit, queries):
+    assert limit > queries, 'assumption'
+    qlimit = QueryBatchLimit(**{name: limit})
+    qlimit.handle_results(
+        results=wrapped_between_irrelevant_results(name, queries),
+        context=None)
     assert True  # no exception raised
 
 
-def test_when_above_limit_it_fails_with_meaningful_error_message():
-    limit = QueryBatchLimit(total=2)
-    with pytest.raises(ValueError) as excinfo:
-        limit.handle_results(results=[qcr(3)], context=None)
-    assert 'Too many (3) queries (limit: 2)' == str(excinfo.value)
+above_limits_params = (
+    'name,limit,queries', [
+        ('total', 10, 11), ('total', 2, 4),
+        ('read', 0, 2), ('read', 4, 7),
+        ('write', 3, 6), ('write', 1, 2),
+    ]
+)
 
 
-def test_given_context_it_is_included_in_error_message():
-    limit = QueryBatchLimit(total=3)
+@pytest.mark.parametrize(*above_limits_params)
+def test_when_above_limit_it_fails_with_meaningful_error_message(name,
+                                                                 limit,
+                                                                 queries):
+    assert limit < queries, 'assumption'
+    qlimit = QueryBatchLimit(**{name: limit})
     with pytest.raises(ValueError) as excinfo:
-        limit.handle_results(
-            results=[qcr(4)], context={'extra': 'context'})
-    assert 'Too many (4) queries (limit: 3) {\'extra\': \'context\'}' == \
-        str(excinfo.value)
+        qlimit.handle_results(
+            results=wrapped_between_irrelevant_results(name, queries),
+            context=None)
+    assert 'Too many ({}) queries (limit: {})'.format(queries, limit) \
+        == str(excinfo.value)
+
+
+@pytest.mark.parametrize(*above_limits_params)
+def test_given_context_it_is_included_in_error_message(name, limit, queries):
+    qlimit = QueryBatchLimit(**{name: limit})
+    with pytest.raises(ValueError) as excinfo:
+        qlimit.handle_results(
+            results=wrapped_between_irrelevant_results(name, queries),
+            context={'extra': 'context'})
+    expected = 'Too many ({}) queries (limit: {}) '\
+               '{{\'extra\': \'context\'}}'.format(queries, limit)
+    assert expected == str(excinfo.value)
 
 
 def test_integration_test_with_db(db):
