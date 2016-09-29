d86410f0a5103b7c041f3d3f324b97e37d788f82
Peter <peter.zsoldos@gmail.com>
2016-09-29 12:48:12 +0200

    simplify test of checking arguments

--- tests/testapp/tests/test_limit_violation_error.py
+++ tests/testapp/tests/test_limit_violation_error.py
@@ -2,19 +2,10 @@ import pytest
 from django_performance_testing.core import LimitViolationError
 
 
-def test_required_arguments_are_limit_actual_name_and_context():
-    with pytest.raises(TypeError):
-        LimitViolationError(
-            limit=3, name='foo', context=None)  # missing actual
-    with pytest.raises(TypeError):
-        LimitViolationError(
-            actual=3, name='foo', context=None)  # missing limit
-    with pytest.raises(TypeError):
-        LimitViolationError(
-            actual=2, name='foo', limit=0)  # missing context
-    with pytest.raises(TypeError):
-        LimitViolationError(
-            actual=2, limit=0, context={})  # missing name
+def test_required_arguments():
+    code = LimitViolationError.__init__.__code__
+    assert ('self', 'name', 'limit', 'actual', 'context', 'tb') == \
+        code.co_varnames[:code.co_argcount]
     lve = LimitViolationError(
         limit=1, actual=2, name='foo', context={'foo': 'bar'})
     assert lve is not None
