1e6db3edc12ef4effaa7c85d5cc0dba85ccb50dc
Martin Bauer <martin.bauer@paessler.com>
2016-11-03 20:44:59 +0100

    more sane naming

--- tests/testapp/tests/test_worst_report_integrates_with_django_testrunner.py
+++ tests/testapp/tests/test_worst_report_integrates_with_django_testrunner.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import Group
 from django_performance_testing.reports import WorstReport
 from django_performance_testing.signals import results_collected
-from testapp.test_helpers import WithId, RunnerFixture
+from testapp.test_helpers import WithId, RunnerTestCasePackage
 import pytest
 import unittest
 
 
 @pytest.fixture
-def make_runner(db):
+def packaged_runner(db):
     class SampleTestCase(unittest.TestCase):
 
         def test_whatever_one(self):
@@ -23,51 +23,50 @@ def make_runner(db):
         def test_slow_query(self):
             list(Group.objects.all())
 
-    def make_runner_with_options(options=None):
+    def get_packaged_runner_with_options(options=None):
         options = options or {}
-        return RunnerFixture(SampleTestCase, nr_of_tests=3,
-                             runner_options=options)
-    return make_runner_with_options
+        return RunnerTestCasePackage(SampleTestCase, nr_of_tests=3,
+                                     runner_options=options)
+    return get_packaged_runner_with_options
 
 
-def test_runner_has_worst_report_attribute(make_runner):
-    runner_fixture = make_runner()
-    runner_fixture.run()
-    assert isinstance(runner_fixture.test_runner.djpt_worst_report,
-                      WorstReport)
+def test_runner_has_worst_report_attribute(packaged_runner):
+    test_package = packaged_runner()
+    test_package.run()
+    assert isinstance(test_package.test_runner.djpt_worst_report, WorstReport)
 
 
-def test_report_is_printed_after_test_is_run(make_runner):
-    runner_fixture = make_runner()
-    _result, output = runner_fixture.run()
+def test_report_is_printed_after_test_is_run(packaged_runner):
+    test_package = packaged_runner()
+    _result, output = test_package.run()
     assert output.endswith(
-        runner_fixture.test_runner.djpt_worst_report.rendered())
+        test_package.test_runner.djpt_worst_report.rendered())
 
 
-def test_no_report_is_printed_with_print_report_set_to_false(make_runner):
-    test_runner = make_runner({"print_report": False})
-    _result, output = test_runner.run()
+def test_no_report_is_printed_with_print_report_set_to_false(packaged_runner):
+    test_package = packaged_runner({"print_report": False})
+    _result, output = test_package.run()
     assert output.find("Worst Performing Items") is -1, \
         "Output was: {}".format(output)
 
 
-def test_after_running_django_testcases_report_is_printed(make_runner):
-    whatever = get_report_value_for(make_runner(), 'whatever', '')
+def test_after_running_django_testcases_report_is_printed(packaged_runner):
+    whatever = get_report_value_for(packaged_runner(), 'whatever', '')
     assert whatever.value == 2
     assert whatever.context == {'test': 'two'}
 
 
-def test_has_worst_test_method_in_the_report(make_runner):
-    report_data = get_report_data(make_runner())
+def test_has_worst_test_method_in_the_report(packaged_runner):
+    report_data = get_report_data(packaged_runner())
     assert 'test method' in report_data
 
 
-def get_report_value_for(runner_fixture, heading, item_name):
-    report_data = get_report_data(runner_fixture)
+def get_report_value_for(runner, heading, item_name):
+    report_data = get_report_data(runner)
     assert heading in list(report_data.keys())
     return report_data[heading]['type name'][item_name]
 
 
-def get_report_data(runner_fixture):
-    runner_fixture.run()
-    return runner_fixture.test_runner.djpt_worst_report.data
+def get_report_data(packaged_runner):
+    packaged_runner.run()
+    return packaged_runner.test_runner.djpt_worst_report.data
